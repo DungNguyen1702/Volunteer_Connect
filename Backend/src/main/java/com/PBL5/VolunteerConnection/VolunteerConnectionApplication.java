46f8dbb75e91f47e939163a0416f7b0ec394ebf1
Nguyên Khôi <115073243+SuKay712@users.noreply.github.com>
2024-04-11 21:09:00 +0700

    add dependency for project

---
+++ Backend/src/main/java/com/PBL5/VolunteerConnection/VolunteerConnectionApplication.java
@@ -0,0 +1,14 @@
+package com.PBL5.VolunteerConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerConnectionApplication.class, args);
+	}
+
+}
+	
---
+++ Backend/src/test/java/com/PBL5/VolunteerConnection/VolunteerConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PBL5.VolunteerConnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
