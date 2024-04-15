e156c7037236452f3223542a0a8803df8052c6d2
Nguyên Khôi <115073243+SuKay712@users.noreply.github.com>
2024-04-16 01:02:13 +0700

    update controller Login, setup thymeleaf

--- Backend/src/main/java/com/PBL5/VolunteerConnection/controller/LoginController.java
+++ Backend/src/main/java/com/PBL5/VolunteerConnection/controller/LoginController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/v1")
 public class LoginController {
     @GetMapping("/login")
