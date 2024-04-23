93774268acf715172766f546e12789951576396e
Nguyên Khôi <115073243+SuKay712@users.noreply.github.com>
2024-04-24 00:10:30 +0700

    <3

--- Backend/src/main/java/com/PBL5/VolunteerConnection/service/JwtService.java
+++ Backend/src/main/java/com/PBL5/VolunteerConnection/service/JwtService.java
@@ -2,13 +2,14 @@ package com.PBL5.VolunteerConnection.service;
 
 import com.PBL5.VolunteerConnection.model.Account;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Date;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -30,5 +31,15 @@ public class JwtService {
                 .withExpiresAt(new Date(System.currentTimeMillis() + 70*60*1000))
                 .sign(algorithm);
 
+    }
+    public Map<String, String[]>verifyToken(String token){
+        Map<String, String[]> user = new HashMap<>();
+        Algorithm algorithm = Algorithm.HMAC256(SECRECT_KEY.getBytes());
+        JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+        DecodedJWT decodedJWT = jwtVerifier.verify(token);
+        String account = decodedJWT.getSubject();
+        String[] roles = decodedJWT.getClaim("roles").asArray(String.class);
+        user.put(account, roles);
+        return user;
     }
 }
