84e39292ca989064e778416bd73ec24c8285e12c
DungNguyen1702 <vandung17022003@gmail.com>
2024-05-10 14:05:09 +0700

    .

--- Frontend/src/api/postAPI.js
+++ Frontend/src/api/postAPI.js
@@ -5,6 +5,10 @@ const postAPI = {
         const url = '/api/v1/post/guest'
         return axiosClient.applicationNoAuth.get(url)
     },
+    getPostDetail: (id) => {
+        const url = `/api/v1/post/postDetail?id=${id}`
+        return axiosClient.applicationNoAuth.get(url)
+    },
 }
 
 export default postAPI;
