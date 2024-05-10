0e09ec88206401d1761d2885774676866c4fa076
DungNguyen1702 <vandung17022003@gmail.com>
2024-05-10 16:59:02 +0700

    .

--- Frontend/src/components/UserProfile/UserSidebar/UserSidebar.js
+++ Frontend/src/components/UserProfile/UserSidebar/UserSidebar.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./UserSidebar.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
 const UserSidebar = ({ activepage }) => {
-    
-    const {account} = useAuth();
+    const { account, setAccount, setToken } = useAuth();
+
+    const navigate = useNavigate();
+
+    const handlerClickLogout = () => {
+        const callFunction = async () => {
+            await setAccount(null);
+            await setToken(null);
+            localStorage.setItem("account", null);
+            localStorage.setItem("token", null);
+            navigate("/user-homepage");
+        };
+        callFunction();
+    };
 
     return (
         <div className="usersidebar">
@@ -65,7 +77,32 @@ const UserSidebar = ({ activepage }) => {
                     </div>
                 </Link>
             }
-            {
+            { account && parseInt(account.role) !== 3 && (
+                <Link to="/Profile/likedPosts" className="styleone">
+                    <div
+                        className={`s1 ${
+                            activepage === "likedPosts" && "active-page"
+                        }`}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9"
+                            />
+                        </svg>
+                        <span>Liked posts</span>
+                    </div>
+                </Link>
+            )}
+            { account && parseInt(account.role) === 1 && (
                 <Link to="/Profile/certificate" className="styleone">
                     <div
                         className={`s1 ${
@@ -89,6 +126,25 @@ const UserSidebar = ({ activepage }) => {
                         <span>Certificate</span>
                     </div>
                 </Link>
+            )}
+            {
+                <div className={`s1`} onClick={handlerClickLogout}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9"
+                        />
+                    </svg>
+                    <span>Logout</span>
+                </div>
             }
         </div>
     );
--- Frontend/src/components/header/dropdown/index.jsx
+++ Frontend/src/components/header/dropdown/index.jsx
@@ -1,36 +1,44 @@
-import { BookOutlined, HeartOutlined, LogoutOutlined, SettingOutlined, UserOutlined } from "@ant-design/icons";
+import {
+    BookOutlined,
+    HeartOutlined,
+    LogoutOutlined,
+    SettingOutlined,
+    UserOutlined,
+} from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
 
 import("./index.scss");
 
-const useDropdownNavigation = ()=>{
+const useDropdownNavigation = () => {
     const navigate = useNavigate();
-    const {setToken, setAccount} = useAuth();
-    
-    const getItemDropDownAccount = (role)=>{
+    const { setToken, setAccount } = useAuth();
+
+    const getItemDropDownAccount = (role) => {
         // console.log(role);
-    
-        const handlerClickLogout = ()=>{
-            const callFunction = async()=>{
+
+        const handlerClickLogout = () => {
+            const callFunction = async () => {
                 await setAccount(null);
                 await setToken(null);
                 localStorage.setItem("account", null);
                 localStorage.setItem("token", null);
-                navigate('/user-homepage');
+                navigate("/user-homepage");
             };
             callFunction();
-        }
-        
+        };
+
         const itemsCandidate = [
             {
                 key: "Candidate-dropdown-1",
                 label: (
-                    <div 
+                    <div
                         class="item-wrapper"
-                        onClick={()=>{navigate('/profile/personInfo')}}
+                        onClick={() => {
+                            navigate("/profile/accountsetting");
+                        }}
                     >
-                        <UserOutlined className="item-icon"/>
+                        <UserOutlined className="item-icon" />
                         <p class="item-title">Person info</p>
                     </div>
                 ),
@@ -38,11 +46,13 @@ const useDropdownNavigation = ()=>{
             {
                 key: "Candidate-dropdown-2",
                 label: (
-                    <div 
+                    <div
                         class="item-wrapper"
-                        onClick={()=>{navigate('/profile/security')}}
+                        onClick={() => {
+                            navigate("/profile/security");
+                        }}
                     >
-                        <SettingOutlined className="item-icon"/>
+                        <SettingOutlined className="item-icon" />
                         <p class="item-title">Security</p>
                     </div>
                 ),
@@ -50,11 +60,13 @@ const useDropdownNavigation = ()=>{
             {
                 key: "Candidate-dropdown-3",
                 label: (
-                    <div 
+                    <div
                         class="item-wrapper"
-                        onClick={()=>{navigate('/profile/likedPosts')}}
+                        onClick={() => {
+                            navigate("/profile/likedPosts");
+                        }}
                     >
-                        <HeartOutlined className="item-icon"/>
+                        <HeartOutlined className="item-icon" />
                         <p class="item-title">Liked posts</p>
                     </div>
                 ),
@@ -62,9 +74,11 @@ const useDropdownNavigation = ()=>{
             {
                 key: "Candidate-dropdown-4",
                 label: (
-                    <div 
+                    <div
                         class="item-wrapper"
-                        onClick={()=>{navigate('/profile/certificates')}}
+                        onClick={() => {
+                            navigate("/profile/certificates");
+                        }}
                     >
                         <BookOutlined className="item-icon" />
                         <p class="item-title">Certificates</p>
@@ -74,25 +88,25 @@ const useDropdownNavigation = ()=>{
             {
                 key: "Candidate-dropdown-5",
                 label: (
-                    <div 
-                        class="item-wrapper" 
-                        onClick={handlerClickLogout}>
-                        <LogoutOutlined className="item-icon"/>
+                    <div class="item-wrapper" onClick={handlerClickLogout}>
+                        <LogoutOutlined className="item-icon" />
                         <p class="item-title">Logout</p>
                     </div>
                 ),
             },
         ];
-    
+
         const itemsOrganization = [
             {
                 key: "Organization-dropdown-1",
                 label: (
-                    <div 
+                    <div
                         class="item-wrapper"
-                        onClick={()=>{navigate('/profile/personInfo')}}
+                        onClick={() => {
+                            navigate("/profile/accountsetting");
+                        }}
                     >
-                        <UserOutlined className="item-icon"/>
+                        <UserOutlined className="item-icon" />
                         <p class="item-title">Person info</p>
                     </div>
                 ),
@@ -100,11 +114,13 @@ const useDropdownNavigation = ()=>{
             {
                 key: "Organization-dropdown-2",
                 label: (
-                    <div 
+                    <div
                         class="item-wrapper"
-                        onClick={()=>{navigate('/profile/security')}}
+                        onClick={() => {
+                            navigate("/profile/security");
+                        }}
                     >
-                        <SettingOutlined className="item-icon"/>
+                        <SettingOutlined className="item-icon" />
                         <p class="item-title">Security</p>
                     </div>
                 ),
@@ -112,11 +128,13 @@ const useDropdownNavigation = ()=>{
             {
                 key: "Organization-dropdown-3",
                 label: (
-                    <div 
+                    <div
                         class="item-wrapper"
-                        onClick={()=>{navigate('/profile/likedPosts')}}
+                        onClick={() => {
+                            navigate("/profile/likedPosts");
+                        }}
                     >
-                        <HeartOutlined className="item-icon"/>
+                        <HeartOutlined className="item-icon" />
                         <p class="item-title">Liked posts</p>
                     </div>
                 ),
@@ -131,16 +149,18 @@ const useDropdownNavigation = ()=>{
                 ),
             },
         ];
-    
+
         const itemsAdmin = [
             {
                 key: "Admin-dropdown-1",
                 label: (
-                    <div 
+                    <div
                         class="item-wrapper"
-                        onClick={()=>{navigate('/profile/personInfo')}}
+                        onClick={() => {
+                            navigate("/profile/accountsetting");
+                        }}
                     >
-                        <UserOutlined className="item-icon"/>
+                        <UserOutlined className="item-icon" />
                         <p class="item-title">Person info</p>
                     </div>
                 ),
@@ -148,11 +168,13 @@ const useDropdownNavigation = ()=>{
             {
                 key: "Admin-dropdown-2",
                 label: (
-                    <div 
+                    <div
                         class="item-wrapper"
-                        onClick={()=>{navigate('/profile/security')}}
+                        onClick={() => {
+                            navigate("/profile/security");
+                        }}
                     >
-                        <SettingOutlined className="item-icon"/>
+                        <SettingOutlined className="item-icon" />
                         <p class="item-title">Security</p>
                     </div>
                 ),
@@ -161,13 +183,13 @@ const useDropdownNavigation = ()=>{
                 key: "Admin-dropdown-3",
                 label: (
                     <div class="item-wrapper" onClick={handlerClickLogout}>
-                        <LogoutOutlined className="item-icon"/>
+                        <LogoutOutlined className="item-icon" />
                         <p class="item-title">Logout</p>
                     </div>
                 ),
             },
         ];
-    
+
         switch (role) {
             case 1:
                 return itemsCandidate;
@@ -180,23 +202,30 @@ const useDropdownNavigation = ()=>{
         }
     };
 
-    const onClickPost = (id)=>{
-        navigate(`/post-detail/${id}`)
+    const onClickPost = (id) => {
+        navigate(`/post-detail/${id}`);
     };
 
-    const getItemDropDownSearchPost = (filterPosts)=>{
-        return filterPosts.map(post => ({
-            key : post.id,
-            label : (
-                <div class='search-post-item' onClick={()=>onClickPost(post.id)}>
-                    <img alt={`post ${post.id} img`} src={post.image} class='search-post-image'/>
-                    <p class='search-post-item-title'>{post.title}</p>
+    const getItemDropDownSearchPost = (filterPosts) => {
+        return filterPosts.map((post) => ({
+            key: post.id,
+            label: (
+                <div
+                    class="search-post-item"
+                    onClick={() => onClickPost(post.id)}
+                >
+                    <img
+                        alt={`post ${post.id} img`}
+                        src={post.image}
+                        class="search-post-image"
+                    />
+                    <p class="search-post-item-title">{post.title}</p>
                 </div>
-            )
+            ),
         }));
-    }
+    };
 
-    return {getItemDropDownAccount, getItemDropDownSearchPost}
-}
+    return { getItemDropDownAccount, getItemDropDownSearchPost };
+};
 
 export default useDropdownNavigation;
