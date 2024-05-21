9973ee368d4cbaeb3befc79306301f71df9376f5
Pham Duy Tin <Phamtin482003@gmail.com>
2024-05-21 14:21:59 +0700

    crud task comment

--- Backend/src/main/java/com/PBL5/VolunteerConnection/controller/TaskCommentController.java
+++ Backend/src/main/java/com/PBL5/VolunteerConnection/controller/TaskCommentController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.PBL5.VolunteerConnection.model.TaskComment;
+import com.PBL5.VolunteerConnection.request.TaskCommentRequest;
+import com.PBL5.VolunteerConnection.response.StatusResponse;
 import com.PBL5.VolunteerConnection.service.TaskCommentService;
 
 @RestController
@@ -16,6 +21,27 @@ import com.PBL5.VolunteerConnection.service.TaskCommentService;
 public class TaskCommentController {
     @Autowired
     private TaskCommentService taskCommentService;
+
+    @PostMapping("/create")
+    ResponseEntity<StatusResponse> createTaskComment(@RequestHeader("Authorization") String token,
+            @RequestBody TaskCommentRequest TaskCommentRequest) {
+        token = token.substring("Bearer ".length());
+        return ResponseEntity.ok(taskCommentService.createTaskComment(token, TaskCommentRequest));
+    }
+
+    @PostMapping("/update")
+    ResponseEntity<StatusResponse> updateTaskComment(@RequestHeader("Authorization") String token,
+            @RequestBody TaskCommentRequest TaskCommentRequest) {
+        token = token.substring("Bearer ".length());
+        return ResponseEntity.ok(taskCommentService.updateTaskComment(token, TaskCommentRequest));
+    }
+
+    @PostMapping("/delete")
+    ResponseEntity<StatusResponse> deleteTaskComment(@RequestHeader("Authorization") String token,
+            @RequestBody TaskCommentRequest TaskCommentRequest) {
+        token = token.substring("Bearer ".length());
+        return ResponseEntity.ok(taskCommentService.deleteTaskComment(token, TaskCommentRequest));
+    }
 
     @GetMapping("/selectAll")
     ResponseEntity<List<TaskComment>> selectAll() {
