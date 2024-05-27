6b2395fc151b2a192c47eaddaa2756d6634279bb
DungNguyen1702 <vandung17022003@gmail.com>
2024-05-27 20:44:11 +0700

    😍 done lan 1

--- Frontend/src/api/activityAPI.js
+++ Frontend/src/api/activityAPI.js
@@ -1,39 +1,37 @@
-import axiosClient from './axiosClient'
+import axiosClient from "./axiosClient";
 
 const activityAPI = {
     getAllActivityByCandidate: () => {
-        const url = '/api/v1/activity/candidate/selectAllActivity'
-        return axiosClient.application.get(url)
+        const url = "/api/v1/activity/candidate/selectAllActivity";
+        return axiosClient.application.get(url);
     },
     getAllActivityByOrganization: () => {
-        const url = '/api/v1/activity/organization/getAllActivity'
-        return axiosClient.application.get(url)
+        const url = "/api/v1/activity/organization/getAllActivity";
+        return axiosClient.application.get(url);
     },
     getActivityDetail: (id) => {
-        const url = `/api/v1/activity/getActivityDetail?activityId=${id}`
-        return axiosClient.application.get(url)
+        const url = `/api/v1/activity/getActivityDetail?activityId=${id}`;
+        return axiosClient.application.get(url);
     },
     getAllActivityByAdmin: () => {
-        const url = `api/v1/admin/getAllActivity`
-        return axiosClient.application.get(url)
+        const url = `api/v1/admin/getAllActivity`;
+        return axiosClient.application.get(url);
     },
-    createActivity : (newAct)=>{
-        const url = `/api/v1/activity/organization/create`
-        return axiosClient.application.post(url, newAct)
+    createActivity: (newAct) => {
+        const url = `/api/v1/activity/organization/create`;
+        return axiosClient.application.post(url, newAct);
     },
-    updateActivity : (updateAct)=>{
-        const url = `/api/v1/activity/organization/update`
-        return axiosClient.application.post(url, updateAct)
+    updateActivity: (updateAct) => {
+        const url = `/api/v1/activity/organization/update`;
+        return axiosClient.application.post(url, updateAct);
     },
     deleteActivity: (actId, isDeleted) => {
-        
         const url = `/api/v1/admin/deleteActivity`;
         return axiosClient.application.post(url, {
             id: actId,
-            isDeleted : isDeleted ? true : false
+            isDeleted: isDeleted ? true : false,
         });
     },
-    
-}
+};
 
 export default activityAPI;
